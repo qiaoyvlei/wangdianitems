a023517982fc026a466680d8f7b1c2476f672ca2
panglinmade <2992926617@qq.com>
2019-05-05 20:37:13 +0800

    改好之后

--- js/homePage2.js
+++ js/homePage2.js
@@ -1,7 +1,7 @@
 $(function () {
     //判断是否登录状态
     var userId = getCookie("userId");
-    if(userId){
+    if(1){
         $('.logined').css('display','inline-block');
         $('.unlogin').css('display','none');
     }else{
